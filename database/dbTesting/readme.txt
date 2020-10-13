6405c488e26d1c0cf5cd528492688dd073e27b4d
NolanCustodio <nac53@njit.edu>
2020-10-13 12:05:36 -0400

    Database scripting is working, the readme.txt explains the command and the .sql files hold the user table

---
+++ database/dbTesting/readme.txt
@@ -0,0 +1,6 @@
+command to run back-up scripts
+
+mysql -u USERNAME -p DATABASE < file.sql
+
+Example:
+sudo mysql -u root -p testDB2 <  userTable.sql
---
+++ database/dbTesting/userTable.sql
@@ -0,0 +1,7 @@
+create table newsBoxUsers(
+	userID		INT		AUTO_INCREMENT	NOT NULL,
+	username	VARCHAR(255)	NOT NULL,
+	password	VARCHAR(255)	NOT NULL,
+	PRIMARY KEY (userID)	
+)
+;
