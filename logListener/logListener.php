e0977bafa9dfa163ffe88ea6722a38d28abf5f0a
jcs78 <j7sanchez13@gmail.com>
2020-10-14 18:48:33 -0400

    Changed code inside 'rabbitMQLib.inc' to stop client class from creating a new queue and exchange to sent information back to client, which allowed the log listener to function locally. In addition, added tons of notes to help explain steps taken inside the 'logListener.php' (receives error logs from RabbitMQ) and 'logSpeaker.php' (sends error logs through RabbitMQ).

--- logListener/logListener.php
+++ logListener/logListener.php
@@ -1,24 +1,36 @@
 #!/usr/bin/php
 <?php
 
+// Requiring once the path to external libraries (not used right now)...
 require_once('path.inc');
+
+// ...the info of the user accessing Rabbit (along with the other things inside 'logRabbit.ini')...
 require_once('get_host_info.inc');
+
+// ...and the classes and functions that allow for connection to RabbitMQ. [-jcs78]
 require_once('rabbitMQLib.inc');
 
+// Create a server using the information inside 'logRabbitMQ.ini.' [-jcs78]
 $logServer = new rabbitMQServer("logRabbitMQ.ini", "logServer");
 
 echo "Now Listening for Logs...".PHP_EOL;
-$logServer->process_logs('detectLogs');
+
+// Grab anything send through the log queue and exchange and run it through the storeLogs function. [-jcs78]
+$logServer->process_logs('storeLogs');
+
 echo "Stopped Listening for Logs.".PHP_EOL;
+
 exit();
 
-function detectLogs($request)
+function storeLogs($request)
 {
+// Open up the file that stores the logs receive (log.txt), append it inside, and close it up. [-jcs78]
 	$fp = fopen('log.txt', 'a');
 	fwrite($fp, $request . "\n");
 	fclose($fp);
 
-	echo $request;
+// Used as a test to ensure the latest log(s) sent were received by the "server"-side. [-jcs78]
+//	echo $request;
 }
 
 ?>
