28a17fa4ab82e79abaf0b39980bf71639ba08021
nmaster009 <nigelmstr@gmail.com>
2020-10-15 23:20:22 -0400

    This will be used to bring all top headlines, output of this will be saved and sent automatically to dB for live updates for top headlines page (cronjob)

---
+++ dmzAPI/curly2.php
@@ -0,0 +1,28 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://newsapi.org/v2/top-headlines?country=us&apiKey=118fb194014e44768263ba6d227f6d93",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Basic MTE4ZmIxOTQwMTRlNDQ3NjgyNjNiYTZkMjI3ZjZkOTM6SVQ0OTAtQ3liZXJTdXJ2aXZvcnM=",
+    "Cookie: __cfduid=db0071484aab34612646d125413c443901601846480"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+?> 
+
+
+
