238e1051062c70a35704bd36299ff5ef3c568fcd
jcs78 <j7sanchez13@gmail.com>
2020-11-02 19:57:32 -0500

    Doubled checked several other files that rabbit Lib was integrated into (testing with them still needs to be done).

--- database/wsRabbitMQListener.php
+++ database/wsRabbitMQListener.php
@@ -1,13 +1,13 @@
 #!/usr/bin/php
 <?php
+
 require_once('path.inc');
 require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
+require_once('databaseRabbitMQLib.php');
 require_once('wsMainControl.php');
 
-//Log Stuff
-//require_once('logRabbitMQLib.inc');
-
+error_reporting(E_ALL);
+set_error_handler("handleError");
 
 function requestProcessor($request)
 {
@@ -18,11 +18,11 @@ function requestProcessor($request)
 	//echo "inside function \n";
   	//var_dump($request);
 
-  	if(!isset($request['type']))
+  	if (!isset($request['type']))
   	{
    		return "ERROR: unsupported message type";
   	}
-  	switch ($request['type'])
+	switch ($request['type'])
   	{
 		case "register":
 	        	try{
@@ -59,49 +59,25 @@ function requestProcessor($request)
 	//return array("returnCode" => '0', 'message'=>"Server received request and processed");
 	//return "Not a valid Case";
 }
-/*
-//Log Stuff
-error_reporting(E_ALL);
-set_error_handler("handleError");
 
-$clientLog = new rabbitMQClient("logRabbitMQ.ini", "logServer");
-$throwableError = "";
+//Try Catch Function For Testing Logs
+try
+{
+	$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
-try {   
-// Causes a 'Throwable Error' (used as a test to see if throwable errors are caught and s$
-
-	//DELETE THIS
-	nonExistent();
-
-	//place any code that needs to be error checked and logged
-
+	echo "testRabbitMQServer BEGIN".PHP_EOL;
+	$server->process_requests('requestProcessor');
+	echo "testRabbitMQServer END".PHP_EOL;
 }
-catch (Throwable $e) {
-// Using the individual pieces of info caught about the error, a string is stored inside $
-        $throwableError = "Throwable Error Caught at " . date("h:i:sa") . " on "  . date($
+catch (Throwable $e)
+{
+        $throwableError = "Throwable Error Caught at " . date("h:i:sa") . " on "  . date("m-d-Y") . ": " . $e->getMessage() . " inside " . $e->getFile()  . " on line " . $e->getLine() . ".\n";
 
-// Sends the content inside that string variable to be shot through the log exchange and $
         $clientLog->send_log($throwableError);
-
-	//This can be used as a test
-
-// Used as a test to ensure the latest log(s) were sent to the log listener(s). [-jcs78]
-//      echo $throwableError;
-
-//DELETE THIS
-//      Causes an error that is caught by handler (used as a test to see if errors are ha$
         echo "Trace: " . $e->getTrace() . "\n";
 }
-*/
 
-//
-
-
-//Main Function - this can be moved into the try/catch block
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
-
-echo "testRabbitMQServer BEGIN".PHP_EOL;
-$server->process_requests('requestProcessor');
-echo "testRabbitMQServer END".PHP_EOL;
 exit();
+
 ?>
+
