9147196da7ef5dd0948f5ecd77ef24e0e55c5e1a
nmaster009 <nigelmstr@gmail.com>
2020-12-14 22:20:34 -0500

    single quotes instead of double qutoes, lets see if it makes a difference

--- dmzAPI/getArticles/techarticles.php
+++ dmzAPI/getArticles/techarticles.php
@@ -14,17 +14,17 @@ ob_start();
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=118fb194014e44768263ba6d227f6d93",
+  CURLOPT_URL => 'https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=118fb194014e44768263ba6d227f6d93',
   CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
+  CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 0,
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_CUSTOMREQUEST => 'GET',
   CURLOPT_HTTPHEADER => array(
-    "Authorization: Basic MTE4ZmIxOTQwMTRlNDQ3NjgyNjNiYTZkMjI3ZjZkOTM6SVQ0OTAtQ3liZXJTdXJ2aXZvcnM=",
-    "Cookie: __cfduid=db0071484aab34612646d125413c443901601846480"
+    'Authorization: Basic MTE4ZmIxOTQwMTRlNDQ3NjgyNjNiYTZkMjI3ZjZkOTM6SVQ0OTAtQ3liZXJTdXJ2aXZvcnM=',
+    'Cookie: __cfduid=db0071484aab34612646d125413c443901601846480'
   ),
 ));
 
