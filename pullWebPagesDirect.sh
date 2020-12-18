c9b308ef894da8e48088e1ad15117d6ef0d013cf
jcs78 <j7sanchez13@gmail.com>
2020-12-18 02:12:53 -0500

    Ability to push a zipped up webPages directory to the Deployment System, pull it from said system and unzip it to the appropriate place achieved.

--- pullWebPagesDirect.sh
+++ pullWebPagesDirect.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-scp jcs78@127.0.0.1:/home/jcs78/git/news-box-app/deploySystem/vmBuilds/buildsWebPages/buildLimbo/webPageZip_Deploy.tar.gz ~/git/news-box-app/localZipStorage
+scp jcs78@127.0.0.1:/home/jcs78/git/news-box-app/deploySystem/vmBuilds/buildsWebPages/buildLimbo/webPagesZip_Deploy.tar.gz ~/git/news-box-app/remoteZipStorage
 
-tar -xvzf ~/git/news-box-app/webPageZip_Deploy.tar.gz -C ~/git/news-box-app
+rm -r webPages/
+
+tar -xvzf ~/git/news-box-app/remoteZipStorage/webPagesZip_Deploy.tar.gz -C ~/git/news-box-app
 
--- pushWebPagesDirect.sh
+++ pushWebPagesDirect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tar -czvf webPageZip_Deploy.tar.gz webPages
+tar -czvf webPagesZip_Deploy.tar.gz webPages
 
-mv ~/git/news-box-app/webPageZip_Deploy.tar.gz ~/git/news-box-app/localZipStorage
+mv ~/git/news-box-app/webPagesZip_Deploy.tar.gz ~/git/news-box-app/localZipStorage
 
-scp ~/git/news-box-app/webPagesZip_Deploy.tar.gz jcs78@127.0.0.1:/home/jcs78/git/news-box-app/deploySystem/vmBuilds/buildsWebPages/buildLimbo
+scp ~/git/news-box-app/localZipStorage/webPagesZip_Deploy.tar.gz jcs78@127.0.0.1:/home/jcs78/git/news-box-app/deploySystem/vmBuilds/buildsWebPages/buildLimbo
 
