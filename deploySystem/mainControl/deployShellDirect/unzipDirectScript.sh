c359ed3d2e9c9d976211aa3a1666c84a51f1c879
jcs78 <j7sanchez13@gmail.com>
2020-12-17 23:21:37 -0500

    Successfully created a mini version of the deployment system.

--- deploySystem/mainControl/deployShellDirect/unzipDirectScript.sh
+++ deploySystem/mainControl/deployShellDirect/unzipDirectScript.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-scp jcs78@10.192.227.91:/home/jcs78/git/news-box-app/deploySystem/mainControl/zipStorageRemote/directToDeploy_zip.tar.gz ~/git/news-box-app/deploySystem/mainControl/unzipStorageRemote
+scp jcs78@127.0.0.1:/home/jcs78/git/news-box-app/deploySystem/mainControl/zipStorageRemote/directToDeploy_zip.tar.gz ~/git/news-box-app/deploySystem/mainControl/unzipStorageRemote
 
 tar -xvzf ~/git/news-box-app/deploySystem/mainControl/unzipStorageRemote/directToDeploy_zip.tar.gz -C ~/git/news-box-app/deploySystem/mainControl/unzipStorageLocal/
-
-#mv ~/git/news-box-app/deploySystem/mainControl/unzipStorageRemote/zipTestDirect ~/git/news-box-app/deploySystem/mainControl/unzipStorageLocal
 
--- deploySystem/mainControl/deployShellDirect/zipDirectScript.sh
+++ deploySystem/mainControl/deployShellDirect/zipDirectScript.sh
@@ -4,5 +4,5 @@ tar -czvf directToDeploy_zip.tar.gz zipTestDirect
 
 mv ~/git/news-box-app/deploySystem/mainControl/deployShellDirect/directToDeploy_zip.tar.gz ~/git/news-box-app/deploySystem/mainControl/zipStorageLocal
 
-scp ~/git/news-box-app/deploySystem/mainControl/zipStorageLocal/directToDeploy_zip.tar.gz jcs78@10.192.227.91:/home/jcs78/git/news-box-app/deploySystem/mainControl/zipStorageRemote
+scp ~/git/news-box-app/deploySystem/mainControl/zipStorageLocal/directToDeploy_zip.tar.gz jcs78@127.0.0.1:/home/jcs78/git/news-box-app/deploySystem/mainControl/zipStorageRemote
 
