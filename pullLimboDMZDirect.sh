dca629cb04ca81a597cdfe5920b4731497a58d6a
jcs78 <j7sanchez13@gmail.com>
2020-12-18 10:30:28 -0500

    Deployment System is able to fully able to fullfill its purpose over multiple VMs.

--- pullLimboDMZDirect.sh
+++ pullLimboDMZDirect.sh
@@ -2,7 +2,6 @@
 
 scp jcs78@10.192.224.108:/home/jcs78/git/news-box-app/deploySystem/vmBuilds/buildsDMZAPI/buildLimbo/dmzZip_Deploy.tar.gz ./remoteZipStorage
 
-
 rm -r dmzAPI/
 
 tar -xvzf ./dmzZip_Deploy.tar.gz -C ./
--- pushDMZDirect.sh
+++ pushDMZDirect.sh
@@ -2,7 +2,6 @@
 
 tar -czvf dmzZip_Deploy.tar.gz dmzAPI
 
-
 mv ./dmzZip_Deploy.tar.gz ./localZipStorage
 
 scp ./localZipStorage/dmzZip_Deploy.tar.gz jcs78@10.192.224.108:/home/jcs78/git/news-box-app/deploySystem/vmBuilds/buildsDMZAPI/buildLimbo
